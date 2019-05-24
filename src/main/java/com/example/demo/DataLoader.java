cc0e04547514ef976ba36ca108038ebf2a4c644f
kellel14 <kellel14@yahoo.com>
2019-05-24 15:40:20 -0400

    Bull Horn (correct project)

---
+++ src/main/java/com/example/demo/DataLoader.java
@@ -0,0 +1,24 @@
+//package com.example.demo;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class DataLoader implements CommandLineRunner {
+//
+//        @Autowired
+//        MessageRepository repository;
+//
+//        @Override
+//        public void run(String... strings) throws Exception{
+//            Message message = new Message(Food);
+//            repository.save(message);
+//
+//            message = new Message();
+//            repository.save(message);
+//
+//            message = new Message();
+//            repository.save(message);
+//        }
+//}
